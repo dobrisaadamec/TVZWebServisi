641ed9c844689919e1e8b5cb77c91ac37f612c28
dobrisaadamec <dadamec@citus.hr>
2016-10-12 13:34:13 +0200

    Priprema za Secure DEMO - start = kopija 1:1 dosadašnjeg WSMovies

---
+++ Demos/MoviesWS_Secure_Start/MoviesWS.HostCmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesWS.HostCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MoviesWS.ServiceSOAP.Public), new Uri("http://127.0.0.1:8989/Movies")))
+            {
+                host.AddServiceEndpoint(typeof(MoviesWS.ServiceSOAP.IPublic), new BasicHttpBinding(), "Public");
+
+                host.Open();
+
+                Console.WriteLine("Evo se vrtim na http://127.0.0.1:8989/Movies");
+                Console.ReadLine();
+            }
+        }
+    } 
+}
