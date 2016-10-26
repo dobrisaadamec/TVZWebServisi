d30b3f486cea06e4c2efbb8a73a7ed98051a9745
dobrisaadamec <dadamec@citus.hr>
2016-10-26 12:52:09 +0200

    web api demo FINISH

---
+++ Demos/Citus.Education.WebApp.Sample_FINISH/Citus.Education.WebApp.Sample.WebAPI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Citus.Education.WebApp.Sample.WebAPI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Demos/Citus.Education.WebApp.Sample_FINISH/Citus.Education.WebApp.Sample.WebAPI/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace Citus.Education.WebApp.Sample.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
